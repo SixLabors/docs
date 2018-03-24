6aeae54d58128d46194a72ea7a1646280cc2ece9
Anton Firszov <antonfir@gmail.com>
2018-03-24 23:30:05 +0100

    add build-dev.cmd + build-dev.ps1

---
+++ build-dev.ps1
@@ -0,0 +1,9 @@
+# Ensure all submodules are checked out, but do not look for latest tags. (Useful for docs development.)
+git submodule update --init
+
+Get-ChildItem ./ext -Directory | ForEach-Object {
+    #$path = $_.FullName
+    #git -C "$path" fetch
+}
+
+docfx
