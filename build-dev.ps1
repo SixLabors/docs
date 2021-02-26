b2f2114e4f59aa9a34c7aa3a5e1489da6df3468a
James Jackson-South <james_south@hotmail.com>
2021-02-26 11:58:58 +0000

    Cache LFS files

--- build-dev.ps1
+++ build-dev.ps1
@@ -3,7 +3,10 @@ git submodule update --init --recursive
 git submodule foreach git pull origin master --recurse-submodules
 
 # Ensure deterministic builds do not affect submodule build
-# TODO: Remove this once all projects are updated to latest build props.
+# TODO: Remove first two values once all projects are updated to latest build props.
 $env:CI = $false
+$env:GITHUB_ACTIONS = $false
+
+$env:SIXLABORS_TESTING = $true
 
 docfx
