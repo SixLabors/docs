2ad3dc50c2d5d067792e2c7923ad5d10b84f2515
James Jackson-South <james_south@hotmail.com>
2022-02-01 23:47:00 +1100

    Update build-dev.ps1

--- build-dev.ps1
+++ build-dev.ps1
@@ -1,6 +1,11 @@
 # Ensure all submodules are checked out with the latest master. (Useful for docs development.)
 git submodule update --init --recursive
-git submodule foreach git pull origin master --recurse-submodules
+
+# Enable the following lines should there be any issues.
+# git submodule foreach git rm --cached -r .
+# git submodule foreach git reset --hard origin/master
+
+git submodule foreach git pull -f origin master --recurse-submodules
 
 # Ensure deterministic builds do not affect submodule build
 # TODO: Remove first two values once all projects are updated to latest build props.
