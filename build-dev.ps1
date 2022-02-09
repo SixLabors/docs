47c32d9062755c49eff3438a430b4df3fb01330b
Dirk Lemstra <dirk@lemstra.org>
2022-02-09 07:41:20 +0100

    Renamed master to main.

--- build-dev.ps1
+++ build-dev.ps1
@@ -1,11 +1,11 @@
-# Ensure all submodules are checked out with the latest master. (Useful for docs development.)
+# Ensure all submodules are checked out with the latest main. (Useful for docs development.)
 git submodule update --init --recursive
 
 # Enable the following lines should there be any issues.
 # git submodule foreach git rm --cached -r .
-# git submodule foreach git reset --hard origin/master
+# git submodule foreach git reset --hard origin/main
 
-git submodule foreach git pull -f origin master --recurse-submodules
+git submodule foreach git pull -f origin main --recurse-submodules
 
 # Ensure deterministic builds do not affect submodule build
 # TODO: Remove first two values once all projects are updated to latest build props.
